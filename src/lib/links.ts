d267b19b7a32ce767d6791b2799b2fc46355bcae
Dan Berry <dan@berryde.com>
2023-06-09 23:13:14 +0100

    feat: update linkedin url

--- src/lib/links.ts
+++ src/lib/links.ts
@@ -5,11 +5,11 @@ export type Link = {
 
 export const links: Record<string, Link> = {
 	github: {
-		href: 'https:://github.com/deb1g19',
+		href: 'https://github.com/deb1g19',
 		icon: 'logo-github'
 	},
 	linkedin: {
-		href: 'https://www.linkedin.com/in/deb1g19/',
+		href: 'https://www.linkedin.com/in/berryde/',
 		icon: 'logo-linkedin'
 	},
 	mail: {
