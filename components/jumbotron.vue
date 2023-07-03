596607a535b8db292dfb8aa710fb012ad4e7df9d
Dan Berry <dan@berryde.com>
2023-07-03 21:03:36 +0100

    fix: restore profile pic path

--- components/jumbotron.vue
+++ components/jumbotron.vue
@@ -3,7 +3,7 @@
 <template>
 	<div class="flex flex-col py-8 items-center relative">
 		<img
-			src="pp.webp"
+			src="/pp.webp"
 			class="rounded-full h-40 w-40 z-10 my-5"
 			alt="a portrait of me"
 		/>
